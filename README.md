231f8a62ab308a2c5f64ff018444ef69bef18aa3
Chris Gatewood <chris.gatewood@icg360.com>
2018-03-19 14:38:38 -0700

    Updated README and formatting of b64

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
-# b64 - Base64 Encoder/Decoder Script for CLI
+# b64 - Consistent command-line Base64 Encoder/Decoder program
 
 - Produces HTTP Basic Auth compatible Base64 strings
 
-Basic usage:
+Basic usage
+------------------------------
+
+Encoding a string:
 
 $ ./b64.py -e | --encode "\<string-to-encode\>"
 
+$ echo "string-to-encode" | ./b64 -e | --encode
+
+
+Decoding a string:
+
 $ ./b64.py -d | --decode "\<string-to-decode\>"
 
+$ echo "string-to-decode" | ./b64 -d | --decode
--- b64.py
+++ b64.py
@@ -1,60 +1,68 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 import sys
 import base64
 import re
 
-'''
+"""
 Simple Base64 Encoder/Decoder Program for CLI.
 
 Requires: sys, re, and base64 python modules.
 
 Created by Chris Gatewood <chris.gatewood@icg360.com>
-'''
+"""
 
 __AUTHOR__ = "Chris Gatewood <chris.gatewood@icg360.com>"
 
 
-# Decode function
 def decode(string):
-    return base64.b64decode(string).decode()
+  """ Returns a decoded base64 string"""
+
+  return base64.b64decode(string).decode()
 
 
-# Encode function
 def encode(string):
-    return base64.b64encode(string.encode()).decode()
+  """ Returns an encoded base64 string"""
+
+  return base64.b64encode(string.encode()).decode()
 
 
-# Print usage of the program (probably needs fleshing out since it can do
-# both piping and standard params)
 def usage():
-    print('Usage: b64.py -d|-e|--decode|--encode|decode|encode "<string>"')
+  """ Prints usage of the program """
+
+  print("""
+    Usage:
+      b64.py <-d|-e|--decode|--encode|decode|encode> "<string>"
+
+      echo "<somestring>" | b64.py <-d|-e|--decode|--encode|decode|encode>
+    """)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        usage()
-        sys.exit(1)
+  if len(sys.argv) < 2:
+    print("Error: no argument was passed.")
+    usage()
+    sys.exit(1)
 
-    # Break down the choice to 'e' or 'd' regardless of which form used, 'e'
-    # and 'd' could also be specified on CLI
-    choice = re.sub("-|ncode|ecode", "", sys.argv[1], flags=re.I)
+  # Break down the choice to 'e' or 'd' regardless of which form used, 'e'
+  # and 'd' could also be specified on CLI
+  choice = re.sub("-|ncode|ecode", "", sys.argv[1], flags=re.I)
 
-    # Check if string parameter is piped in using "echo" or something else, if
-    # nothing specified, quit with usage
-    if not sys.stdin.isatty():
-        param = sys.stdin.readline()
-    elif len(sys.argv) == 3:
-        param = sys.argv[2]
-    else:
-        usage()
-        sys.exit(1)
+  # Check if string parameter is piped in using "echo" or something else, if
+  # nothing specified, quit with usage
+  if not sys.stdin.isatty():
+    param = sys.stdin.readline()
+  elif len(sys.argv) == 3:
+    param = sys.argv[2]
+  else:
+    usage()
+    sys.exit(1)
 
-    # Meat and potatoes!
-    if choice == 'e':
-        print(encode(param))
-    elif choice == 'd':
-        print(decode(param))
-    else:
-        usage()
-        sys.exit(1)
+  # Meat and potatoes!
+  if choice == 'e':
+    print(encode(param))
+  elif choice == 'd':
+    print(decode(param))
+  else:
+    usage()
+    sys.exit(1)
